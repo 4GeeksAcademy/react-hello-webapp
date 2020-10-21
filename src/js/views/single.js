addee48e5b001bdb28a6c64433fb5555771fb5e0
Paolo Lucano <44124290+plucodev@users.noreply.github.com>
2020-10-21 17:33:36 -0400

    Update single.js

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const Single = props => {
 	const { store, actions } = useContext(Context);
+	const params = useParams();
 	return (
 		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[props.match.params.theid].title}</h1>
+			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
 
 			<hr className="my-4" />
 

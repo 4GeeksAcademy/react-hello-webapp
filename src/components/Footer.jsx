f3e37590c436e2aae46388f86171905f7d4ef20d
Alejandro Sanchez <aalejo@gmail.com>
2024-05-09 21:55:57 +0000

    better docs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
+			Check the <a target="_blank" href="https://4geeks.com/docs/start/start-react-advanced-project">template documentation</a> <i class="fa-solid fa-file"></i> for help.
+		</p>
+		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
 			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
 		</p>

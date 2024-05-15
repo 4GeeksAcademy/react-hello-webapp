b68ce7773de3746d8cb6bb2df0f0d1987c76dd1e
Alejandro Sanchez <aalejo@gmail.com>
2024-05-15 11:30:08 -0400

    updated last reference to BaseHome to rename it to Layout

--- src/routes.jsx
+++ src/routes.jsx
@@ -16,7 +16,7 @@ export const router = createBrowserRouter(
     // Create your routes here, if you want to keep the Navbar and Footer in all views, add your new routes inside the containing Route.
     // Root, on the contrary, create a sister Route, if you have doubts, try it!
     // Note: keep in mind that errorElement will be the default page when you don't get a route, customize that page to make your project more attractive.
-    // Note: The child paths of the BaseHome element replace the Outlet component with the elements contained in the "element" attribute of these child paths.
+    // Note: The child paths of the Layout element replace the Outlet component with the elements contained in the "element" attribute of these child paths.
 
       // Root Route: All navigation will start from here.
       <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >

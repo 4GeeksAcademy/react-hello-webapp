2629de92f14dca885de50c878476fdfce8c58229
Jonas <jonas.backline@gmail.com>
2025-05-16 11:04:33 +0000

    footer + Contact + AboutUs + routes

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,9 +1,17 @@
+
+import { Link } from "react-router-dom";
+
 export const Footer = () => {
-	return (
-		<div className="text-center">
-			<footer>
-				<h1>Esto es el footer</h1>
-			</footer>
-		</div>
-	)
-}
+    return (
+        <footer className="bg-warning py-3 mt-auto w-100">
+            <div className="container d-flex justify-content-center gap-4">
+                <Link to="/contacto" className="text-dark text-decoration-none">
+                    Contacto
+                </Link>
+                <Link to="/sobre-nosotros" className="text-dark text-decoration-none">
+                    Sobre nosotros
+                </Link>
+            </div>
+        </footer>
+    );
+};

a1ae7b7b50312739f798d66ea67724860790256c
Jonas <jonas.backline@gmail.com>
2025-05-21 16:58:28 +0000

    Initial Store + Store Reducer

--- front/src/store.js
+++ front/src/store.js
@@ -1,18 +1,48 @@
 export const initialStore=()=>{
   return{
+    carrito: []
     
   }
 }
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
-    case 'agregar_al_carrito':
 
-      return {};
+  switch(action.type) {
+    case 'agregar_al_carrito': {
+      const producto = action.payload;
 
-    case 'eliminar_del_carrito':
+      // ¿existe el produto en el carrito?
+      const productoExistente = store.carrito.find(item => item.id === producto.id);
 
-      return {};
+      let nuevoCarrito;
+      if (productoExistente) {
+        //Si existe, aumentamos la cantidad
+        nuevoCarrito = store.carrito.map(item => item.id? {...item,cantidad: item.cantidad + 1}
+          : item
+        );
+      } else {
+        // Si no exite, lo añadimos con cantidad 1
+        nuevoCarrito = [...store.carrito, {...producto,cantidad: 1}];
+      }
+
+      return {
+        ...store, 
+        carrito: nuevoCarrito
+      };
+    }
+    
+
+    case 'eliminar_del_carrito': {
+      const id = action.payload;
+
+      // Filtramos fuera el producto que queremos eliminar
+      const nuevoCarrito = store.carrito.filter(item => item.id !== id)
+
+      return {
+        ...store,
+        carrito: nuevoCarrito
+      };
+    }
 
     default:
       throw Error('No es una accion valida');
